cd662e1be090ae01f2bb3c087f75ec687867ab0b
adrian <adrian@last.fm>
2013-01-10 16:29:52 +0000

    - added missing javadoc - added missing license headers

--- src/main/java/fm/last/commons/lang/exception/LastExceptionUtils.java
+++ src/main/java/fm/last/commons/lang/exception/LastExceptionUtils.java
@@ -1,11 +1,37 @@
+/*
+ * Copyright 2013 Last.fm
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package fm.last.commons.lang.exception;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility methods for dealing with exceptions.
+ */
 public class LastExceptionUtils {
 
+  /**
+   * Extracts a List of Throwable objects that are assignable from the passed type by "walking" the passed Throwable and
+   * checking all nested exceptions.
+   * 
+   * @param throwable The Throwable to extract child exceptions from.
+   * @param type The matching type of nested Throwable to extract.
+   * @return A List of matching nested Throwables.
+   */
   public static List<Throwable> extractNestedThrowablesOfType(Throwable throwable, Class<?> type) {
     List<Throwable> throwables = new ArrayList<Throwable>();
     throwable = throwable.getCause();
